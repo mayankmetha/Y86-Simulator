71a721ad3f74ccc1ffa034e9dfaa48fc351e77f2
mayankmetha <mayankmetha@gmail.com>
2018-12-31 22:14:19 +0530

    added pass 1 assembler

--- assemble.py
+++ assemble.py
@@ -189,9 +189,12 @@ class Y86Assmbler:
                 except:
                     error += 'Line %d: Instruction error.\n' % lineCount
                     continue
-            try:
-                fin.close()
-            except IOError:
-                pass
-            if error != '':
-                self.printError(error)
+        try:
+            fin.close()
+        except IOError:
+            pass
+        if error != '':
+            self.printError(error)
+
+        # pass 2: generate object code
+        
